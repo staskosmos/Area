9cb15d2adbd5c2f56b498ec17f3e6c09af6d67b1
staskosmos <mr.varyn@mail.ru>
2020-04-16 00:39:28 +0300

    add UserRepo

---
+++ src/main/java/com/example/area/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.area.repos;
+
+import com.example.area.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
