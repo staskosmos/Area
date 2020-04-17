8ce65c38eff3bfdf8d87555592448826defe845d
staskosmos <mr.varyn@mail.ru>
2020-04-18 01:35:45 +0300

    add get author

--- src/main/java/com/example/area/domain/Message.java
+++ src/main/java/com/example/area/domain/Message.java
@@ -1,9 +1,6 @@
 package com.example.area.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -11,18 +8,25 @@ public class Message {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     private String text;
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User author;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "<none>";
+    }
 
     public User getAuthor() {
         return author;
@@ -32,12 +36,12 @@ public class Message {
         this.author = author;
     }
 
-    public Message() {
+    public Integer getId() {
+        return id;
     }
 
-    public Message(String text, String tag) {
-        this.text = text;
-        this.tag = tag;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getText() {
--- src/main/java/com/example/area/domain/User.java
+++ src/main/java/com/example/area/domain/User.java
@@ -61,7 +61,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return getRoles();
     }
 
     public String getPassword() {
